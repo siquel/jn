bce36a3bddadf939af57d13991532e781e59a042
siquel <squual@kahvipaussi.net>
2016-09-08 20:46:07 +0300

    Complete first exercise

--- exercises/00-helloworld/helloworld.cpp
+++ exercises/00-helloworld/helloworld.cpp
@@ -2,19 +2,36 @@
 #include <stdio.h>
 #include <jn/thread/thread.h>
 
+/*
+write a program which starts a thread that prints hello world once a second
+*/
+
+static bool s_exit = false;
 
 int32_t func(void*)
 {
-    printf("Hello from thread\n");
-    return 55;
+    while (!s_exit)
+    {
+        printf("Hello World!\n");
+        ::Sleep(1000);
+    }
+    return 0;
+}
+
+BOOL WINAPI sigHandler(DWORD sig)
+{
+    if (sig == CTRL_C_EVENT) s_exit = true;
+
+    return TRUE;
 }
 
 int main(int argc, char** argv)
 {
+    SetConsoleCtrlHandler(sigHandler, TRUE);
+
     jn::Thread thread;
     thread.start(func);
     thread.join();
 
-    printf("Thread exited with code %d\n", thread.getExitCode());
     return 0;
 }
