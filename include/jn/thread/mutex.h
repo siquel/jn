279f61df35c590a25705a677b9660e1d04434186
siquel <squual@kahvipaussi.net>
2016-09-08 13:42:35 +0300

    Add mutex

---
+++ include/jn/thread/mutex.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <windows.h>
+
+namespace jn
+{
+    struct Mutex
+    {
+        Mutex()
+        {
+            InitializeCriticalSection(&m_cs);
+        }
+
+        ~Mutex()
+        {
+            DeleteCriticalSection(&m_cs);
+        }
+
+        void lock()
+        {
+            EnterCriticalSection(&m_cs);
+        }
+
+        void unlock()
+        {
+            LeaveCriticalSection(&m_cs);
+        }
+
+        CRITICAL_SECTION m_cs;
+    private:
+        Mutex(const Mutex&);
+        Mutex& operator=(const Mutex&);
+    };
+
+    struct ScopedMutex
+    {
+        ScopedMutex(Mutex& mutex) 
+            : m_mutex(mutex)
+        {
+            m_mutex.lock();
+        }
+
+        ~ScopedMutex()
+        {
+            m_mutex.unlock();
+        }
+
+    private:
+        Mutex& m_mutex;
+    };
+}
