8d0c0e95837484bd02b08a232c8eaaa5e8e2de8d
siquel <squual@kahvipaussi.net>
2016-09-08 21:02:35 +0300

    Complete exercise 7

---
+++ exercises/01-threethreads/threethreads.cpp
@@ -0,0 +1,53 @@
+#include <jn/jn.h>
+#include <jn/thread/thread.h>
+#include <jn/thread/mutex.h>
+#include <stdio.h>
+/*
+Implement a code that starts three threads that prints "Hello world"
+from thread X", where X=thread id. Take care of protecting critical
+sections!
+*/
+
+static bool s_exit = false;
+
+jn::Mutex stdoutMutex;
+
+int32_t threadProc(void* data)
+{
+    while (!s_exit)
+    {
+        stdoutMutex.lock();
+        printf("Hello world from thread %d\n", *(uint32_t*)data);
+        stdoutMutex.unlock();
+        ::Sleep(100);
+    }
+    return 0;
+}
+
+BOOL WINAPI sigHandler(DWORD sig)
+{
+    if (sig == CTRL_C_EVENT) s_exit = true;
+
+    return TRUE;
+}
+
+int main(int argc, char** argv)
+{
+    SetConsoleCtrlHandler(sigHandler, TRUE);
+
+    const uint32_t numThreads = 3;
+    jn::Thread threads[numThreads];
+    uint32_t ids[numThreads] = {};
+    for (uint32_t i = 0; i < numThreads; ++i)
+    {
+        ids[i] = i;
+        threads[i].start(threadProc, &ids[i]);
+    }
+
+    for (uint32_t i = 0; i < numThreads; ++i)
+    {
+        threads[i].join();
+    }
+
+    return 0;
+}
